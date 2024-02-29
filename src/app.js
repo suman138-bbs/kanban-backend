313a5a112f7c18a832dee7669c8141fda1f18316
STSumanBhandari <suman.bhandari@shridharatechnologies.in>
2024-02-29 23:44:51 +0530

    allow cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://quizzie123.netlify.app", "http://localhost:5173"],
+    origin: ["https://kanbanfrontend1.netlify.app/", "http://localhost:5173"],
     credentials: true,
   })
 );
