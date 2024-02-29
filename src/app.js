d5cfe517efc8af22766f9685d7681bf85f55b97e
STSumanBhandari <suman.bhandari@shridharatechnologies.in>
2024-02-29 23:58:32 +0530

    Allow all origin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://kanbanfrontend1.netlify.app/", "http://localhost:5173"],
+    origin: true,
     credentials: true,
   })
 );
